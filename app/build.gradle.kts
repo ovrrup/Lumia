897ad390ee640da6f26320d09d1eddc8b8d9de8f
ovrrup <ovrrbutuphere@gmail.com>
2026-06-11 00:02:19 +0530

    Updated files.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.scholarsync.xmntpa"
+    applicationId = "com.bas.tard.studies"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
