6a955970780f5723c7ccb83aac9018271df6eb85
ovrrup <ovrrbutuphere@gmail.com>
2026-06-11 18:13:09 +0530

    chore: rename application to Lumia
    Update the application name, ID, and metadata to rebrand as Lumia.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.bas.tard.studies"
+    applicationId = "lumia.foss"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
